0700949fb115226dcc1271e2a18fa0b342990ecf
High School Student <rhs@rlas-macair-38.rlas-116.org>
2018-09-24 11:00:36 -0500

    Initial Commit

---
+++ gitHubSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitHubSample
+//
+//  Created by High School Student on 9/24/18.
+//  Copyright © 2018 High School Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
